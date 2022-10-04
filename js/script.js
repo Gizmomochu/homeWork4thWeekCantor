b46f26aa68f30b29897ea28720e907c40f38b2b0
Krzysiek Jurewicz <k.jurewicz89@gmail.com>
2022-10-04 17:15:17 +0200

    add function and initial reconstruction code

--- js/script.js
+++ js/script.js
@@ -1,36 +1,42 @@
-const welcome = () => {
-    console.log("Witam wszystkich dewsów i proszę o konstruktywną krytykę za którą serdecznie dziękuję :D");
+{
+    const welcome = () => {
+        console.log("Witam wszystkich dewsów i proszę o konstruktywną krytykę za którą serdecznie dziękuję :D");
+    }
+    
+    const init = () => {
+        welcome();
+    }
+    
+    const formElement = document.querySelector(".js-form");
+    const exchangeWant = document.querySelector(".js-form__exchangeWant");
+    const currencyWant = document.querySelector(".js-form__currencyWant");
+    const currency = document.querySelector(".js-form__currency");
+    
+    const displayCurrencyValue = () => {
+        const value = document.querySelector(".js-form__result");
+        const inputValue = document.querySelector(".js-form__input");
+        const exchangeWant = document.querySelector(".js-form__exchangeWant");
+
+        const finalResult = inputValue.value / exchangeWant.value;
+        value.innerText = finalResult.toFixed(2);
+    }
+    
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+        displayCurrencyValue();
+    
+        currencyWant.innerText = exchangeWant.value;
+    
+        if(exchangeWant.value === "4.75") {
+            currency.innerText = "EURO"
+        }
+        else if(exchangeWant.value === "4.90") {
+            currency.innerText = "USD"
+        }
+        else {
+            currency.innerText = "JPN";
+        }
+    });
+    
+    init();
 }
-
-const init = () => {
-    welcome();
-}
-
-let formElement = document.querySelector(".js-form");
-let inputValue = document.querySelector(".js-form__input");
-let exchangeWant = document.querySelector(".js-form__exchangeWant");
-let currencyWant = document.querySelector(".js-form__currencyWant");
-let value = document.querySelector(".js-form__result");
-let currency = document.querySelector(".js-form__currency");
-
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    let finalResult = inputValue.value / exchangeWant.value;
-   
-    currencyWant.innerText = exchangeWant.value;
-
-    value.innerText = finalResult.toFixed(2);
-
-    if(exchangeWant.value === "4.75") {
-        currency.innerText = "EURO"
-    }
-    else if(exchangeWant.value === "4.90") {
-        currency.innerText = "USD"
-    }
-    else {
-        currency.innerText = "JPN";
-    }
-});
-
-init();
