5ab8af84e6a998f5c50015d7a13986cb78c8eb91
Krzysiek Jurewicz <k.jurewicz89@gmail.com>
2022-10-04 17:20:27 +0200

    add function and further reconstruction js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@
     const welcome = () => {
         console.log("Witam wszystkich dewsów i proszę o konstruktywną krytykę za którą serdecznie dziękuję :D");
     }
-    
+
     const init = () => {
         welcome();
     }
-    
+
     const formElement = document.querySelector(".js-form");
-    const exchangeWant = document.querySelector(".js-form__exchangeWant");
-    const currencyWant = document.querySelector(".js-form__currencyWant");
-    const currency = document.querySelector(".js-form__currency");
-    
+    // const exchangeWant = document.querySelector(".js-form__exchangeWant");
+    // const currencyWant = document.querySelector(".js-form__currencyWant");
+    // const currency = document.querySelector(".js-form__currency");
+
     const displayCurrencyValue = () => {
         const value = document.querySelector(".js-form__result");
         const inputValue = document.querySelector(".js-form__input");
@@ -20,23 +20,31 @@
         const finalResult = inputValue.value / exchangeWant.value;
         value.innerText = finalResult.toFixed(2);
     }
-    
-    formElement.addEventListener("submit", (event) => {
-        event.preventDefault();
-        displayCurrencyValue();
-    
+
+    const calculation = () => {
+        const exchangeWant = document.querySelector(".js-form__exchangeWant");
+        const currencyWant = document.querySelector(".js-form__currencyWant");
+        const currency = document.querySelector(".js-form__currency");
+
         currencyWant.innerText = exchangeWant.value;
-    
-        if(exchangeWant.value === "4.75") {
+        if (exchangeWant.value === "4.75") {
             currency.innerText = "EURO"
         }
-        else if(exchangeWant.value === "4.90") {
+        else if (exchangeWant.value === "4.90") {
             currency.innerText = "USD"
         }
         else {
             currency.innerText = "JPN";
         }
+    }
+
+
+
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+        displayCurrencyValue();
+        calculation();
     });
-    
+
     init();
 }
